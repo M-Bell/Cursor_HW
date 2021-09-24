1f573a8586439f915e2e37520f991c5044e94f10
M-Bell <63159095+M-Bell@users.noreply.github.com>
2021-09-24 19:28:11 +0300

    Add files via upload

---
+++ src/HW3/Money.java
@@ -0,0 +1,51 @@
+public class Money {
+    private long bills;
+    private byte coins;
+
+    public Money(long bills, byte coins) {
+        this.bills = bills;
+        this.coins = coins;
+    }
+
+    public Money(double value) {
+        this.bills = (long) value;
+        this.coins = (byte) ((value-bills) * 100);
+    }
+
+    public static Money add(Money val1, Money val2) {
+        return new Money(val1.getNumericValue() + val2.getNumericValue());
+    }
+
+    public static Money subtract(Money val1, Money val2) {
+        return new Money(val1.getNumericValue() - val2.getNumericValue());
+    }
+
+    public static Money multiply(Money val1, Money val2) {
+        return new Money(val1.getNumericValue() * val2.getNumericValue());
+    }
+
+    public static Money divide(Money val1, Money val2) {
+        return new Money(val1.getNumericValue() / val2.getNumericValue());
+    }
+
+    public static boolean more(Money val1, Money val2) {
+        return val1.getNumericValue() > val2.getNumericValue();
+    }
+
+    public static boolean equal(Money val1, Money val2) {
+        return val1.getNumericValue() == val2.getNumericValue();
+    }
+
+    public static boolean less(Money val1, Money val2) {
+        return val1.getNumericValue() < val2.getNumericValue();
+    }
+
+    public double getNumericValue() {
+        return bills + (double) coins / 100;
+    }
+
+    @Override
+    public String toString() {
+        return bills + "," + coins;
+    }
+}
