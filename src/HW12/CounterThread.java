074fca748e84e10f70ae42a804ca632ffe00a2f5
Maksym Shyiko <maxymshyjko@gmail.com>
2021-11-05 20:15:01 +0200

    Task 12

---
+++ src/HW12/CounterThread.java
@@ -0,0 +1,29 @@
+package HW12;
+
+public class CounterThread extends Thread {
+
+    private final Base base;
+    private int number;
+
+    public CounterThread(Base base, int number) {
+        this.base = base;
+        this.number = number;
+
+    }
+
+    @Override
+    public void run() {
+        synchronized (base) {
+            for (int i = 0; i < 10; i++) {
+                base.notify();
+                try {
+                    System.out.println("Thread " + (number % 2 == 0 ? 2 : 1) + ": " + number);
+                    base.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                this.number += 2;
+            }
+        }
+    }
+}
---
+++ src/HW12/Test.java
@@ -0,0 +1,12 @@
+package HW12;
+
+public class Test {
+    public static void main(String[] args) {
+        Base base = new Base();
+        CounterThread ct1 = new CounterThread(base, 1);
+        CounterThread ct2 = new CounterThread(base, 2);
+
+        ct1.start();
+        ct2.start();
+    }
+}
